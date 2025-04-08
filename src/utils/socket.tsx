03f615877ce8c4645284ee58ed9c7c8d49c50f83
Keyla Soriano <44504724+ksoriano55@users.noreply.github.com>
2025-04-08 17:28:09 -0600

    Update socket.tsx

--- src/utils/socket.tsx
+++ src/utils/socket.tsx
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 // const socket = io("http://104.198.227.111:5020");
-const socket = io("http://localhost:5000", {
+const socket = io("https://server-chat-xf66.onrender.com", {
     reconnection: true,
     reconnectionAttempts: Infinity,
     reconnectionDelay: 2000,
